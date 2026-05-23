5440604045ff544f335cb714eb89dfe24991dd69
SHIVAM <shivam72668497@gmail.com>
2026-05-23 13:35:08 +0530

    Create firebase-config.js

---
+++ js/firebase/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYdTJdXbxZ4bUGW-wO1BRaszEUb2Q_BAg",
+  authDomain: "shivearnapp.firebaseapp.com",
+  databaseURL: "https://shivearnapp-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shivearnapp",
+  storageBucket: "shivearnapp.firebasestorage.app",
+  messagingSenderId: "467728812929",
+  appId: "1:467728812929:web:a73f12827309d9c09d7680"
+};
+export default firebaseConfig;
